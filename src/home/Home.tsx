996e48b885c9169aea6dc66fb060d38590d1e1d4
rejanequeiroz <rejanequeiroz24@gmail.com>
2022-11-23 12:30:59 -0300

    atualizçao

---
+++ src/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Home.css'
+
+function Home () {
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+
+        <div className="container">
+        <img className="imagem" src="https://blog.emania.com.br/wp-content/uploads/2015/12/paisagem-tropical-wallpaper-1.jpg" alt="foto de paissagem" />
+
+        </div>
+
+        </>
+    )
+}
+
+export default Home
