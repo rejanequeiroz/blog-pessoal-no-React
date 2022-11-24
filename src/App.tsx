df162db248e0a78d4ac1a21213ec159ad392b3ce
rejanequeiroz <rejanequeiroz24@gmail.com>
2022-11-24 12:28:54 -0300

    atualizaçao

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './home/Home'
+import { Grid } from '@material-ui/core';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 
 function App() {
   return (
     <>
-    <Home/>
+    < Navbar />
+    < Home />
+    < Footer />
     </>
   );
 }
